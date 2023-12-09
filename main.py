c9d6692ddc63ad7a33c6674c3619e2558e339c4c
Sven Brieden <sven.brieden@gmail.com>
2023-12-09 23:42:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from dateutil import tz
 
 utc_dt = datetime.now(timezone.utc)  # UTC time
 ch_dt = utc_dt.astimezone(tz.gettz("Europe/Zurich"))
+print(ch_dt)
 
 with open("timestamps.txt", "a") as f:
     f.write(ch_dt.isoformat() + "\n")
